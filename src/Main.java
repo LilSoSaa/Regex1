edda2a422ba65fedc0bacf6758ebcd4486b71211
giova <giovannitopor@gmail.com>
2023-01-30 19:44:29 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String string1 = "i like to code near the Lake with my friend Luke";
+        string1 = string1.replaceAll("L[^u]ke","BLA");
+        System.out.println(string1);
+
+
+
+    }
+}
